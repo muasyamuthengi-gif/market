95b8a0b186e0bd2ea03b6f8346a0f2763f771da1
muasyamuthengi-gif <muasyamuthengi@gmail.com>
2026-02-07 13:56:28 +0300

    Update Contact.tsx

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -19,7 +19,7 @@ const Contact: React.FC = () => {
               </h3>
               <p className="text-gray-400 text-lg leading-relaxed">
                 123 Elite Way, Fitness District<br />
-                Los Angeles, CA 90210
+                thika, CA 90210
               </p>
               <button className="mt-6 text-primary font-bold hover:underline flex items-center space-x-2">
                 <span>Open in Maps</span>
@@ -38,7 +38,7 @@ const Contact: React.FC = () => {
                   <svg className="w-6 h-6 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <span>(555) 123-4567</span>
+                  <span>07 123-4567</span>
                 </li>
                 <li className="flex items-center space-x-4">
                   <svg className="w-6 h-6 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
